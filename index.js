49739abcc7c8249a15bb4ee54c13160da25a5e65
Gagandeep07_Kaur <gagandeep07kaur@gmail.com>
2020-07-16 09:51:55 +0530

    added code to add person - create operation

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var route = require("./route/route");
 var app = express();
 
 //parse application/json
 app.use(bodyParser.json())
+
 //parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: true}))
-
-
 
 //--route/
 app.get("/",(req,res)=>{
     res.json({msg: "Hello world"});
 })
 
+//-----http://localhost:4000/api*;
+app.use("/api", route);
 
 app.listen(4000,()=>{
     console.log("API started");
 })
+
--- route/route.js
+++ route/route.js
@@ -3,3 +3,21 @@ var bodyParser = require('body-parser');
 var PersonModel = require('../model/personModel');
 var route = express.Router();
 
+
+
+//add person======================================================================
+route.post("/addPerson",(req, res) =>{
+    var person = new PersonModel(req.body);
+
+    person.save().then((doc)=> {
+        //if person added
+        res.json({msg:"Person added"}).status(201);;
+
+	}).catch((error) =>{
+    //if person not added
+     res.json({msg:"Can't add person"}).status(204);
+	})
+})
+
+
+module.exports = route;
